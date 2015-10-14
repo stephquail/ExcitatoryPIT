fb3d723632588286d00a69a5f081c8d7314770ff
Steph Quail <stephanie.quail@gmail.com>
2015-10-14 15:30:11 +1100

    high level script

---
+++ analysis.R
@@ -0,0 +1,6 @@
+###This script runs the data extraction and summary for all stages of the PIT & Devaluation task
+
+source("instru_analysis.R")
+source("pav_analysis.R")
+source("transfer_analysis.R")
+source("deval_analysis.R")
